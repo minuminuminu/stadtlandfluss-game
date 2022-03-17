e522c4e9b6de054b18cde314c7ac3900bf632590
Minu Kim <minu.kim2003@gmail.com>
2022-03-17 16:22:56 +0100

    add routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import TestPage from "./pages/TestPage";
 import { GlobalStyle } from "./GlobalStyles";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
 
 ReactDOM.render(
   <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}></Route>
+        <Route path="/" element={<App />} />
+        <Route path="test" element={<TestPage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
     <GlobalStyle />
